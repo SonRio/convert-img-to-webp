1b423d99b5a78eddb9b6475f8f33f1b0dab9b552
sonnvtOB <sonnvt@uniorbit.org>
2025-03-02 15:53:05 +0700

    fix: update params func

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import imagemin from "imagemin";
 import imageminWebp from "imagemin-webp";
 
-// folder root
-imagemin(["./assets/images/*.{jpg,png}"], {
-  destination: "./assets/webp/", // folder to save image
-  plugins: [imageminWebp({})],
-}).then(() => {
-  console.log("Images Converted Successfully!!!");
-});
+export const converImage = (pathSrc, pathSave) => {
+  // folder root
+  imagemin([pathSrc], {
+    destination: pathSave, // folder to save image
+    plugins: [imageminWebp({})],
+  }).then(() => {
+    console.log("Images Converted Successfully!!!");
+  });
+};
